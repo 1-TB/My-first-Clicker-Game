26f7dde77d87da71a22eea7612162d08b6e796fb
Jordan <jordan11231123@gmail.com>
2019-05-04 01:14:30 -0500

    Adding income !!

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var cookies = 0;
 var cursors = 0;
 var fatpeople = 0;
 var fatgain = 0;
+var income = 0;
 function cookieClick(number){
     cookies = cookies + number;
     document.getElementById("cookies").innerHTML = cookies;
@@ -16,6 +17,7 @@ function buyCursor(){
     }
     var nextCost = Math.floor(10 * Math.pow(1.5,cursors));       
     document.getElementById("cursorCost").innerHTML = nextCost;  
+   
 }
 function buyFatpeople(){
     var fatCost = Math.floor(10000 * Math.pow(1.1,fatpeople)); 
@@ -25,14 +27,21 @@ function buyFatpeople(){
         document.getElementById("fatpeople").innerHTML = fatpeople;
     }
     var nextCost = Math.floor(10000 * Math.pow(1.1,fatpeople));       
-    document.getElementById("fatCost").innerHTML = nextCost;  
+    document.getElementById("fatCost").innerHTML = nextCost; 
+    
 }
 
+function calculateIncome(){
+var temp = fatpeople * 300;
+ var income = temp + cursors;
+    document.getElementById("income").innerHTML = income;
+}
 
 // About to make a loop xD
 
 window.setInterval(function(){
-var fatgain = fatpeople * 100;
+var fatgain = fatpeople * 300;
 cookieClick(cursors);
 cookieClick(fatgain);
+calculateIncome();
 }, 1000);
